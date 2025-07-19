a975caa54a5d62a82baf8ff5884395bd3cbbef85
nickfree4437x <146987759+nickfree4437x@users.noreply.github.com>
2025-07-19 17:08:43 +0530

    Update useBoardData.js

--- client/src/hooks/useBoardData.js
+++ client/src/hooks/useBoardData.js
@@ -11,7 +11,7 @@ export const useBoardData = (token) => {
   const fetchTasks = async () => {
     try {
       setLoading(true);
-      const { data } = await axios.get("http://localhost:5000/api/tasks", {
+      const { data } = await axios.get("https://to-do-borad.onrender.com/api/tasks", {
         headers: { Authorization: `Bearer ${token}` },
       });
       setTasks(data);
@@ -24,7 +24,7 @@ export const useBoardData = (token) => {
 
   const fetchLogs = async () => {
     try {
-      const { data } = await axios.get("http://localhost:5000/api/activity", {
+      const { data } = await axios.get("https://to-do-borad.onrender.com/api/activity", {
         headers: { Authorization: `Bearer ${token}` },
       });
       setLogs(data);
